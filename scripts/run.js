96027afe76c0b27b9342a6a34da72dcf876059bc
Sviatoslav <20727358+svetkrasin@users.noreply.github.com>
2022-01-06 14:27:29 -0800

    Test minting NFTs on local blockchain

--- scripts/run.js
+++ scripts/run.js
@@ -4,14 +4,10 @@ const main = async () => {
 	await nftContract.deployed();
 	console.log("Contract deployed to:", nftContract.address);
 
-	// Call the function.
 	let txn = await nftContract.makeAnEpicNFT()
-	// Wait for it to be mined.
 	await txn.wait()
 
-	// Mint another NFT for fun.
 	txn = await nftContract.makeAnEpicNFT()
-	// Wait for it to be mined.
 	await txn.wait()
 
 };
@@ -25,3 +21,5 @@ const runMain = async () => {
 		process.exit(1);
 	}
 };
+
+runMain();
