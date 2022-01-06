e1a06baf872d0e5697153e8f3bee98abfec6076e
Sviatoslav <20727358+svetkrasin@users.noreply.github.com>
2022-01-06 14:16:18 -0800

    test minting on local blockchain

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -26,7 +26,8 @@ contract MyEpicNFT is ERC721URIStorage {
 		_safeMint(msg.sender, newItemId);
 
 		// Set the NFTs data.
-		_setTokenURI(newItemId, "blah");
+		_setTokenURI(newItemId, "https://jsonkeeper.com/b/4EV8");
+		console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
 		// Increment the counter for when the next NFT is minted.
 		_tokenIds.increment();
